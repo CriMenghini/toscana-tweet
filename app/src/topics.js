3d985de79a820226464a08f4ef83c6d17b521828
CriMenghini <cricri.menghini@gmail.com>
2018-04-17 19:37:07 +0200

    [upd] center text

--- app/src/topics.js
+++ app/src/topics.js
@@ -18,6 +18,7 @@ d3.json("../../data/output/topic_squares.json", function(data) {
         var listHashtag = new Array(data.length)
         var widthX = new Array(data.length)
         var widthY = new Array(data.length)
+        var heightY = new Array(data.length)
 
         for (i=0; i<data.length; i++){
             listHashtag_ = data[i].hashtags
@@ -40,17 +41,12 @@ d3.json("../../data/output/topic_squares.json", function(data) {
                         .attr("height", h);
 
 
-        //var Links = svg.append("a")
-        //               .attr("xlink:href", "http://en.wikipedia.org/wiki/Matteo_Renzi");
-
-
-        
 
         var boxes = svg.selectAll('rect')
                         .data(data)
                         .enter()
                         .append("a")
-                        .attr("xlink:href", "http://en.wikipedia.org/wiki/prova")
+                        .attr("xlink:href", function (d,i){return "http://en.wikipedia.org/wiki/"+topicArray[i];})
                         .append('rect');
 
 
@@ -80,14 +76,7 @@ d3.json("../../data/output/topic_squares.json", function(data) {
 
                                             return coordinata;}
 
-                                            //if (dataset[i].number_tweets + arrayX[i] + dataset[i].number_tweets > w-10){
-                                            //       arrayX[i] = w - (coordinata + dataset[i-1].number_tweets)
-                                            //       return arrayX[i];
 
-                                            //}
-
-                                            //else {arrayX[i] = coordinata
-                                            //        return coordinata;};}
 
                                      else {arrayX[i] =  10
                                             indexRow[i] = indexRow[i-1] + 1
@@ -128,11 +117,30 @@ d3.json("../../data/output/topic_squares.json", function(data) {
                                    return listHashtag[i][j]; }
 
                            ;})
-                           .attr('x', function (d,i){return (arrayX[i] + widthX[i])/2;})
-                           .attr('y', function (d,i){return (arrayY[i] + widthY[i])/2;})
+                           .attr('x', function (d,i){
+                                    if (i==0 ){return (arrayX[i] + widthX[i])/2; }
+
+                                    else if (data[i-1].number_tweets + arrayX[i-1] + 10 + data[i-1].number_tweets < w-10)
+                                        {return (arrayX[i-1] + widthX[i-1] + 10 + arrayX[i] + widthX[i])/2;}
+
+                                     else {return (arrayX[i] + widthX[i])/2}
+                           ;})
+                           .attr('y', function (d,i){
+                                    if (i==0){
+                                    heightY[i] = (arrayY[i] + widthY[i])/2
+                                    return heightY[i] ;}
+
+                                    else if (indexRow[i] == indexRow[i-1])
+                                    {heightY[i] = heightY[i-1]
+                                    return heightY[i];}
+
+                                     else if (indexRow[i] != indexRow[i-1]) {
+                                         heightY[i] = heightY[i-1] + 200
+                                         return (heightY[i-1] + 200);};})
                            .style('font-size', '30px')
                            .attr("font-family", "sans-serif")
 						   .attr("fill", "blue")
-						   .attr("transform", "translate(150,150) rotate(30)");});
+						   .attr("transform", "rotate(0)");});
+
 
 
