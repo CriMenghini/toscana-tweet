5bf4899bce50b37989628492f9c150ce2a36fbd8
CriMenghini <cricri.menghini@gmail.com>
2018-04-24 12:32:03 +0200

    [add] viz top user

--- app/src/topic/viz-RT.js
+++ app/src/topic/viz-RT.js
@@ -2,10 +2,11 @@
 var w = '90%';
 var h = 600;
 
+// Define the SVG
 var svg = d3.select('.viz-retweet')
-                        .append("svg")
-                        .attr("width", w)
-                        .attr("height", h);
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
 
 
 // Declare fake data (2 top tweets)
@@ -72,106 +73,119 @@ var dataset = [
                  'user_id': 130537001}];
 
 
-// Declare ranking
+// Declare ranking (yAxis)
 var data = [0,1,2,3,4,5,6,7,8,9]
 var data2 = [1,2,3,4,5,6,7,8,9,10]
 var dataAltezza = [3,4,5,6,7,8,9,10,11,12]
 
-var xpoint = 430;
+// Define the group for the plot and the point to translate it on the xaxis
+var xTran = 430;
 var g = svg.append("g").attr("transform", "translate(" + 30 + ",40)");
 
+
+// Scale yAxis
 var y0 = d3.scalePoint()
             .domain(data)
             .range([0, h - 400]);
 
-var yPos = new Array(data.length)
 
-
-//var distance = 80;
+// Declare the tooltip div
 var tipTool = d3.select("body")
-	.append("div")  // declare the tooltip div
-	.attr("class", "tooltip")              // apply the 'tooltip' class
-	.style("opacity", 0);
+	            .append("div")  // declare the tooltip div
+	            .attr("class", "tooltip") // apply the 'tooltip' class
+	            .style("opacity", 0);
 
 
+// Append labels to ticks
 g.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(" + xpoint + ",10)")
-    .call(d3.axisLeft(y0).tickSize(0))
-    .selectAll("text")
-    .attr("y", -3)
-    .attr("x", -15)
-    .attr('font-size','15px')
-    .data(dataset)
-    .on('mouseover',  function(d,i) {
+ .attr("class", "axis")
+ .attr("transform", "translate(" + xTran + ",10)")
+ .call(d3.axisLeft(y0).tickSize(0))
+ .selectAll("text")
+ .attr("y", -3)
+ .attr("x", -15)
+ .attr('font-size','15px')
+ .data(dataset) // Declare data to make the plot interactive
+ .on('mouseover', function(d,i) {
+                            // Let the tooltip appear
+                            tipTool.transition()
+				                   .duration(200)
+				                   .style("opacity", 14)
+				                   .style("display", "block") ;
+			                // Fill in the tooltip with html
+			                tipTool.html('<br>' + d.tweet + '<br>Numero di retweet:' + d.num_RT +
+			                             '<br> Autore:' + d.user + '<br> Nr. follower '+d.user_followers +
+			                             '<br>Nr. amici' + d.user_friends)
+				                    .style("right", '600px') // make it appear always in the same place (need to check it again -not in svg)
+				                    .style("top", "900px");
 
-			tipTool.transition()
-				.duration(200)
-				.style("opacity", 14)
-				.style("display", "block") ;
-			tipTool.html('<br>' + d.tweet + '<br>Numero di retweet:' + d.num_RT +
-			            '<br> Autore:' + d.user + '<br> Nr. follower '+d.user_followers + '<br>Nr. amici' + d.user_friends)
-				.style("right", '600px')//d3.event.pageX  + "px")
-				.style("top", "900px");
-            d3.select(this).style('fill','red');
+                            // Color the label when the mouse hovers
+                            d3.select(this).style('fill','red');
 
-            function myfunction(d,i) {return 'rect.class'+i};
+                            // Define function to select and color the row of squares that corresponds to the label
+                            function myfunction(d,i) {return 'rect.class'+i};
 
-			var classNameProva = myfunction(d,i);
+                            // Run the function
+			                var classNameProva = myfunction(d,i);
 
-			d3.selectAll(classNameProva).style('fill','green').attr('height',16)
-    .attr('width',16).attr("transform", "translate(" + 0 + ",-1)");
-			})
-	 .on("mouseout", function(d,i){
+                            // Color the row of squares
+			                d3.selectAll(classNameProva)
+			                  .style('fill','green').attr('height',16)
+                              .attr('width',16)
+                              .attr("transform", "translate(" + 0 + ",-1)");
+                              })
+   .on("mouseout", function(d,i){
+                             tipTool.transition()  //Opacity transition when the tooltip disappears
+                                    .duration(500)
+                                    .style("opacity", "0")
+                                    .style("display", "none");
 
-        //Mouse event
-                tipTool
-                    .transition()  //Opacity transition when the tooltip disappears
-                .duration(500)
-                .style("opacity", "0")
-                .style("display", "none");
-                d3.select(this).style('fill','black');
-                function myfunction(d,i) {return 'rect.class'+i};
+                             // Get the origin color back
+                             d3.select(this).style('fill','black');
 
-			    var classNameProva = myfunction(d,i);
+                             function myfunction(d,i) {return 'rect.class'+i};
 
-                d3.selectAll(classNameProva).style('fill','blue').attr('height',12)
-    .attr('width',12).attr("transform", "translate(" + 0 + ",+1)");
-                  //The tooltip disappears
-      });
+			                 var classNameProva = myfunction(d,i);
+
+                             d3.selectAll(classNameProva)
+                               .style('fill','blue')
+                               .attr('height',12)
+                               .attr('width',12)
+                               .attr("transform", "translate(" + 0 + ",+1)");
+                               });
+
 
 var w1 = '98%';
 var h1 = (h - 20)/data.length*10;
 var indicator = 0
 
 g.selectAll('.bar')
-    .data(dataset)
-    .enter()
-    .append("svg")      // Append one svg per object in dataset
-    .attr("width", w1)
-    .attr("height", h1)
-    .attr('id', function (d,i){return i})
-    .selectAll("rect")
-    .data(function(d,i) {       // Inherit data from svg by mapping of objects
-        // Create an array of number of required rects
-        var rangeArray = d3.range(d.num_RT).map(function(d,i) {return d*10;})
-        var c = [];
-        var trialLL = new Array(rangeArray.length+1).join(i).split('');
-            for (var j = 0; j < rangeArray.length; j++){
-               c.push([rangeArray[j], trialLL[j]]);
-            }
-        return c;
-      })
-    .enter().append("rect")
-    .attr('class',function (d){return 'class'+d[1]})
-    .attr('height',12)
-    .attr('width',12)
-    .attr('x',  function(d) {
-          return xpoint + 20 + d[0]*2;})
-    .attr('y', function (d,i) {return y0(d[1]);})
-    .attr('fill','blue')
-    ;
-    //.attr('y', function (d) {return y0(2);} )
+ .data(dataset)
+ .enter()
+ .append("svg")      // Append one svg per object in dataset
+ .attr("width", w1)
+ .attr("height", h1)
+ .attr('id', function (d,i){return i})
+ .selectAll("rect")
+ .data(function(d,i) {       // Inherit data from svg by mapping of objects
+                 // Create an array of number of required rects
+                 var rangeArray = d3.range(d.num_RT).map(function(d,i) {return d*10;})
+                 var c = [];
+                 var trialLL = new Array(rangeArray.length+1).join(i).split('');
+                 for (var j = 0; j < rangeArray.length; j++){
+                          c.push([rangeArray[j], trialLL[j]]);}
+                 return c;})
+
+ .enter().append("rect")
+ .attr('class',function (d){return 'class'+d[1];})
+ .attr('height',12)
+ .attr('width',12)
+ .attr('x',  function(d) {return xTran + 20 + d[0]*2;})
+ .attr('y', function (d,i) {return y0(d[1]);})
+ .attr('fill','blue');
+
+
+
 
 
 
---
+++ app/src/topic/viz-user.js
@@ -0,0 +1,117 @@
+// Set up SVG
+var w = 900;
+var h = 500;
+
+
+var svg = d3.select('.viz-user')
+                        .append("svg")
+                        .attr("width", w)
+                        .attr("height", h);
+
+var yTrans = 10;
+var xTrans = 0;
+var g = svg.append("g").attr("transform", "translate(" + 100 + "," + yTrans +")");
+
+
+// Use indexOf to fade in one by one
+var utenti = ['User1','User2','User3','User4','User5','User6','User7','User8','User9','User10'];
+var info = ['num_tweet','num_foll','num_frien','num_pos','num_neg']
+
+
+// Load data
+d3.csv("data_user.csv", function(error, data) {
+      if (error) throw error;
+
+      var parsedData = [];
+      data.forEach((d) => {
+        var dObj = {utente: d.utente, ranks: []};
+        for (var information in d) {
+          if (information != "utente") {
+            if (d[information] != 0) {
+            	dObj.ranks.push({information: information, rank: +d[information], utente: dObj});
+            }
+          }
+        }
+        parsedData.push(dObj);
+      });
+
+console.log(parsedData)
+
+var xScale = d3.scalePoint()
+          .domain(info)
+          .range([0, w-150]);
+
+var xAxis = d3.axisBottom(xScale)
+             .scale(xScale)
+             .tickSize(0);
+
+g.append("g")
+ .attr("class", "x axis")
+ .attr("transform", "translate(0,850)")
+ .call(xAxis)
+ .selectAll("text")
+ .attr("y", 10)
+ .attr("x", 0)
+ .attr('font-size','15px');
+
+
+
+var yScale = d3.scalePoint()
+                .domain(utenti)
+                // Definisci il valore dell'altezza in cui è definito svg
+                .range([0,h-50]);
+
+var yAxis = d3.axisLeft(yScale)
+             .scale(yScale)
+             .tickSize(0);
+
+g.append("g")
+ .attr("class", "y axis")
+ .attr("transform", "translate(0,0)")
+ .call(yAxis)
+ .selectAll("text")
+ .attr("y", 0)
+ .attr("x", -20)
+ .attr('font-size','15px');
+
+
+var lineFunction = d3.line()
+                         .x(function(d) { return d.x; })
+                         .y(function(d) { return d.y; })
+                         .curve(d3.curveLinear);
+
+var circles = g.selectAll("path-lines")
+                  .data(parsedData)
+                  .enter()
+                  .append("path")
+                  .attr('d', function (d) {toReturn = [];
+                                                        for (var j=0; j<d.ranks.length; j++){
+                                                                 toPush = {'y':yScale('User' + d.ranks[j].rank),
+                                                                           'x': xScale(d.ranks[j].information)}
+                                                                 toReturn.push(toPush);}
+
+                                                        return lineFunction(toReturn);})
+                  .attr('stroke','grey')
+                  .attr("stroke-opacity", .1)
+                  .attr("stroke-width", 5)
+                  .attr("fill", "none")
+                  .on('mouseover', function (d) {d3.select(this)
+                                                   .transition()           // apply a transition
+                                                   .duration(0)
+                                                   .attr('class','active')
+                                                   .attr('stroke','blue')
+                                                   .attr('stroke-opacity',.8)})
+
+                  .on('mouseout', function (d) {d3.select(this)
+                                                  .attr('stroke', 'grey')
+                                                  .attr('stroke-opacity', .1)
+                                                  .classed('active', false)})
+
+                  .on('click', function (d) {d3.select(this)
+                                                .attr('class', 'clicked')
+                                                .attr('stroke','green')
+                                                .attr('stroke-opacity',.8)
+                                                });
+
+
+});
